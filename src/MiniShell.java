3acca40564f56b5f38b3999583c3e7c23e670839
jtcbutler <jbutler@pugetsound.edu>
2025-02-19 09:45:54 -0800

    Reworking controller

--- src/MiniShell.java
+++ src/MiniShell.java
@@ -1,12 +1,20 @@
 import org.fusesource.jansi.AnsiConsole;
 import org.fusesource.jansi.Ansi;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Arrays;
 
 //System.setProperty("user.dir", "/"); note: does not change working directory, but does change the globar variable
 public class MiniShell{
 	private static final String PROMPT = "msh> ";
+	private final HashMap<String, ShellCommand> commandMap;
 
 	public MiniShell(){
+
+		commandMap = new HashMap<>();	
+		commandMap.put("clear", new Clear());
+		commandMap.put("pwd", new Pwd());
+
 		AnsiConsole.systemInstall();
 		inputLoop();
 		AnsiConsole.systemInstall();
@@ -14,41 +22,44 @@ public class MiniShell{
 
 	private void inputLoop(){
 		Scanner scanner = new Scanner(System.in);
-		boolean proceed = true;
 
-		while(proceed){
+		while(true){
 			System.out.print(PROMPT);
 
 			String[] inputSegments = (splitAndTrim(scanner.nextLine()));
 
-			try{
-				switch(inputSegments[0]){
-					case "exit": 
-					proceed = false;
-					System.out.println("exiting...");
-					break;
-
-					case "clear":
-					ShellCommand clear = new Clear();
-					System.out.print(clear.execute());
-					break;
-
-					case "pwd":
-					ShellCommand pwd = new Pwd();
-					System.out.print(pwd.execute());
-					break;
-
-					default:
-					throw new ShellCommand.ShellCommandException("minish: " + inputSegments[0] + ": command not found");
-				}
+			if(inputSegments[0].equals("exit")){
+				System.out.println("exiting...");
+				break;
 			}
-			catch(ShellCommand.ShellCommandException e){
-				System.out.print(Ansi.ansi().cursorUp(1).fgRed().render(PROMPT).fgDefault().cursorDown(1).cursorLeft(PROMPT.length()));
-				System.out.println(e.getMessage());
+			else{
+				try{
+					execute(inputSegments);
+				}
+				catch(ShellCommand.ShellCommandException e){
+					System.out.print(Ansi.ansi().cursorUp(1).fgRed().render(PROMPT).fgDefault().cursorDown(1).cursorLeft(PROMPT.length()));
+					System.out.println(e.getMessage());
+				}
 			}
 		}
 
 		scanner.close();
+	}
+
+	private void execute(String[] command) throws ShellCommand.ShellCommandException {
+		ShellCommand executable = this.commandMap.get(command[0]);
+
+		if(executable == null){
+			throw new ShellCommand.ShellCommandException("msh: " + command[0] + ": command not found");
+		}
+		else{
+			executable.setArguments(Arrays.copyOfRange(command, 1, command.length));
+		}
+
+		System.out.println(executable.execute());
+	}
+
+	private void executePipe(String[][] commands){
 	}
 
 	private String[] splitAndTrim(String input){
--- src/ShellCommand.java
+++ src/ShellCommand.java
@@ -1,7 +1,12 @@
 abstract public class ShellCommand{
 	protected String pipedInput;
+	protected String[] arguments;
 
 	abstract public String execute() throws ShellCommandException;
+
+	public void setArguments(String[] arguments){
+		this.arguments = arguments;
+	}
 
 	public void setPipedInput(String pipedInput){
 		this.pipedInput = pipedInput;
