691da2052b88034fafd3f6d5fcbcd5ae86cf119b
jtcbutler <jbutler@pugetsound.edu>
2025-02-25 22:07:12 -0800

    Fully completed ShellException.java

--- src/ShellException.java
+++ src/ShellException.java
@@ -1,4 +1,17 @@
+/**
+ * A barebones exception type used throughout the MiniShell to improve clarity
+ *
+ * @authors Jackson Butler, Jiafeng
+ * @date	Feb 25, 2025
+ */
 public class ShellException extends Exception {
+
+	/**
+	 * Create a new ShellException with an explanation of what went wrong
+	 *
+	 * @param message an explanation of what went wrong
+	 * @return ShellException the newly created ShellException
+	*/
 	public ShellException(String message) {
 		super(message);
 	}
