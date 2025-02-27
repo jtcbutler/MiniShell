1924789583e042439aea99f1c481c473a1b08369
jtcbutler <jbutler@pugetsound.edu>
2025-02-26 19:14:22 -0800

    Fully completed Pwd.java

--- src/Pwd.java
+++ src/Pwd.java
@@ -1,16 +1,33 @@
 import org.fusesource.jansi.Ansi;
 
+/**
+ * A stripped down version of the Bash command 'cp'
+ *
+ * @author	Jackson Butler
+ * @date 	Feb 26, 2025
+ */
 public class Pwd extends ShellCommand {
+
+	/**
+	 * Create a new Pwd command
+	 */
+	public Pwd(){}
+
+	/**
+	 * Retrieve the String path of the current working directory
+	 *
+	 * @return the path of the current working directory
+	 */
 	@Override
 	protected String processCommand() throws ShellException {
-		if(arguments.length > 0 && (arguments[0].equals("--help") || arguments[0].equals("-h"))){
-			return help();
-		}
-		else{
-			return System.getProperty("user.dir") + "\n";
-		}
+		return System.getProperty("user.dir") + "\n";
 	}
 
+	/**
+	 * Return text explaining the intended use of this command
+	 *
+	 * @return String the explanation
+	*/
 	protected String help(){
 		return Ansi.ansi().fgYellow().render(""
 		+ "pwd: pwd\n"
